78c69983c71a161e7e2319d60137cb4352965eb3
Musty419 <125439935+Musty419@users.noreply.github.com>
2023-03-18 14:42:49 +0100

    My commit
    Commit my task

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,12 +9,12 @@
 
 int main(void)
 {
-int n;
+int n,last_digit;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 
-int last_digit = n % 10;
+last_digit = n % 10;
 
 printf("Last digit of %d is ", n);
 
@@ -30,6 +30,5 @@ else
 {
 printf("%d and is less than 6 and not 0\n", last_digit);
 }
-
 return (0);
 }
